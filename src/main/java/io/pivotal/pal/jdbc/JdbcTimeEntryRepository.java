6a6a419be73c0dd7671f8ad26bce21c7e6440a90
Raymond Fang <rfang@pivotal.io>
2019-02-22 15:03:58 +0800

    Initial with mysql connection

---
+++ src/main/java/io/pivotal/pal/jdbc/JdbcTimeEntryRepository.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class JdbcTimeEntryRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTimeEntryRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public TimeEntry find(Long id) {
+        return jdbcTemplate.queryForObject("SELECT id, project_id, user_id, date, hours FROM time_entries WHERE id = ?",
+                new Object[]{id}, mapper);
+    }
+
+    private final RowMapper<TimeEntry> mapper = (rs, rowNum) ->
+         new TimeEntry(
+                            rs.getLong("id"),
+                            rs.getLong("project_id"),
+                            rs.getLong("user_id"),
+                            rs.getDate("date"),
+                            rs.getInt("hours")
+                    );
+
+}
---
+++ src/main/java/io/pivotal/pal/jdbc/TimeEntryController.java
@@ -0,0 +1,30 @@
+package io.pivotal.pal.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+    private JdbcTimeEntryRepository timeEntriesRepo;
+
+    @Autowired
+    public TimeEntryController(JdbcTimeEntryRepository timeEntriesRepo) {
+        this.timeEntriesRepo = timeEntriesRepo;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
+        TimeEntry timeEntry = timeEntriesRepo.find(id);
+        if (timeEntry != null) {
+            return new ResponseEntity<>(timeEntry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
