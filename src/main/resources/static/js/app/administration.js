e3a41704ddbf104093f184b19fa63ed2b059c65e
Raymond Fang <rfang@pivotal.io>
2019-05-08 13:00:55 +0800

    progress icon added

--- src/main/resources/static/js/app/administration.js
+++ src/main/resources/static/js/app/administration.js
@@ -1,22 +1,26 @@
 myApp.controller('adminController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
     console.log("adminController start...!");
-    $scope.uploadFile = function(file) {
-    file.upload = Upload.upload({
-      url: '/admin/uploadFile',
-      data: {file: file},
-    });
+    $scope.progressHide=true;
 
-    file.upload.then(function (response) {
-      $timeout(function () {
-        file.result = response.data;
-      });
-    }, function (response) {
-      if (response.status > 0)
-        $scope.errorMsg = response.status + ': ' + response.data;
-    }, function (evt) {
-      // Math.min is to fix IE which reports 200% sometimes
-      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });
+    $scope.uploadFile = function(file) {
+        $scope.progressHide=false;
+        file.upload = Upload.upload({
+          url: '/admin/uploadFile',
+          data: {file: file},
+        });
+
+        file.upload.then(function (response) {
+          $timeout(function () {
+            file.result = response.data;
+            $scope.progressHide=true;
+          });
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
     }
 }]);
 
