13f1feaeb80b7766ddcb05b6e74aca1f6b2428b4
Raymond Fang <rfang@pivotal.io>
2019-05-10 16:56:03 +0800

    Repo test done, can test with empy data

--- src/test/java/io/pivotal/pal/cert/CertExamRepositoryTest.java
+++ src/test/java/io/pivotal/pal/cert/CertExamRepositoryTest.java
@@ -4,12 +4,16 @@ package io.pivotal.pal.cert;
 import io.pivotal.pal.cert.exam.CertExamRecord;
 import io.pivotal.pal.cert.exam.CertExamRepository;
 import io.pivotal.pal.cert.exam.CertExamSummary;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +29,11 @@ import static org.assertj.core.api.Assertions.*;
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @Import(CertExamRepository.class) //Somehow @DataJdbcTest not able to load @Repository, Import to force it happen
-@Transactional //This will cause each test rollback after testing
+@Transactional //This will cause each test rollback after testing, also rollback the beforeTestRun.sql
+@SqlGroup({
+        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforeTestRun.sql"),
+       // @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:afterTestRun.sql")
+})
 public class CertExamRepositoryTest {
 
     @Autowired
@@ -34,40 +42,40 @@ public class CertExamRepositoryTest {
 
     @Test
     public void getCertExamSummary() {
-        List<CertExamSummary> en = repo.getCertExamSummary("2012-01-01","2018-12-31 24:00:00");
+        List<CertExamSummary> en = repo.getCertExamSummary("2019-01-01","2019-01-02");
         System.out.println("=====>"+en.size());
         for (CertExamSummary es: en) {
             System.out.println("=====>"+es.getPivotalExamCode()+":"+es.getPassCount()+":"+es.getTotal());
         }
-        assertThat(en).size().isGreaterThan(1);
+        assertThat(en).size().isGreaterThan(0);
      }
 
      @Test
     public void getRegionCertExamSummary() {
-        List<CertExamSummary> en = repo.getCertExamSummaryByRegion("2012-01-01","2018-12-31","EMEA");
+        List<CertExamSummary> en = repo.getCertExamSummaryByRegion("2019-01-01","2019-01-02","APAC");
         System.out.println(en.size());
-        assertThat(en).size().isGreaterThan(1);
+        assertThat(en).size().isGreaterThan(0);
     }
 
     @Test
     public void getCertExamRecords() {
-        List<CertExamRecord> result = repo.getCertExamRecords("2012-01-01","2018-12-31",50);
+        List<CertExamRecord> result = repo.getCertExamRecords("2019-01-01","2019-01-02",50);
         System.out.println(result.size());
-        assertThat(result).size().isEqualTo(50);
+        assertThat(result).size().isGreaterThan(0);
     }
 
 
     @Test
     public void getDynamicQueryResult() {
-        String sql = "select candidate_email, score from  cert_exam_result  limit 10 ";
+        String sql = "select CANDIDATE_EMAIL from  CERT_EXAM_VW  where EXAM_CENTER='TEST' ";
         List<HashMap> result =  repo.getDynamicQueryResult(sql);
-        System.out.println("===>"+result.get(1).get("candidate_email"));
-        assertThat(result).size().isEqualTo(10);
+        System.out.println("===>"+result.get(0).get("CANDIDATE_EMAIL"));
+        assertThat(result).size().isEqualTo(1);
     }
 
     @Test
     public void insertBath() {
-        //Test 1
+        //Test 1 OLD: for olde CERT_EXAM_RESULT --> PEARSON VUE DATA, DEPREATE SOON
         List<CertExamRecord> records = new ArrayList<>(1);
         CertExamRecord record= new CertExamRecord();
         record.setDataSource("PearTest2");
@@ -89,7 +97,7 @@ public class CertExamRepositoryTest {
         int[] result = repo.insertBatch(records);
         assertThat(result[0]==1);
 
-        //Test 2
+        //Test 2: NEW: FOR CERT_EXAM -->PSI DATA
         List<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
 
         LinkedHashMap<String, String> single = new LinkedHashMap<>();
@@ -106,15 +114,17 @@ public class CertExamRepositoryTest {
 
     @Test
     public void allDynamicTab() {
-        int result =  repo.addDynamicTab("dTabTest","My Report","SELECT select site_country, candidate_email, exam_code, score, grade from  cert_exam_result \n" +
-                "where site_country='Singapore' limit 10");
+        int result =  repo.addDynamicTab("dTabTest","My Report","SELECT EXAM_NAME, CANDIDATE_EMAIL FROM CERT_EXAM \n" +
+                "where EXAM_CENTER='TEST'");
        assertThat(result).isEqualTo(1);
+
 
         List<String> result1 = repo.getDynamicTabIDs();
         assertThat(result1).size().isGreaterThan(0);
 
+
         HashMap result2 = repo.getDynamicTabByID("dTabTest");
-        assertThat(result2).size().isGreaterThan(0);
+        assertThat(result2).size().isGreaterThan(1);
 
         result = repo.deleteDynamicTab("dTabTest");
         assertThat(result).isEqualTo(1);
@@ -123,9 +133,10 @@ public class CertExamRepositoryTest {
 
     @Test
     public void getRoleByEmail() {
-        String result = repo.getRoleByEmail("rfang@pivotal.io");
-        System.out.println(result);
-        assertThat(result).isEqualTo("ADMIN");
+        String result = repo.getRoleByEmail("TEST@TEST.COM");
+        System.out.println("===>"+result);
+        assertThat(result).isEqualTo("TESTER");
     }
+
 
 }
